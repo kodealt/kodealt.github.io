60d25ebbe3f423a9a10ec3c7316d09cf0c7bf580
kodealt <shineyubhandari9@gmail.com>
2026-08-15 23:54:57 -0400

    add effects, test favicon

---
+++ effextor/favi.js
@@ -0,0 +1,28 @@
+export function logos(faviconInterval = 100, toggleIsTrue = true) {
+       let j = 0;
+       subFolder = 'f3';
+       
+
+       setLogo(`/assets/${subFolder}/00${(j%frames+1).toString().padStart(2,'0')}.png`);   
+
+       const logoInterval = setInterval(() => {
+           if (!toggleIsTrue) {
+               clearInterval(logoInterval);
+                return;
+           }
+       setLogo(`/assets/${subFolder}/00${(j%frames+1).toString().padStart(2,'0')}.png`);
+       j++;
+       }, faviconInterval);
+
+}
+
+function setLogo(url, size = "64x64") {
+    document.querySelectorAll("link[rel~='icon']").forEach(link => link.remove());
+    
+    const link = document.createElement('link');
+    link.rel = 'icon';
+    link.type = 'image/png';
+    link.href = url;
+    link.sizes = size;
+    document.head.appendChild(link);
+}
