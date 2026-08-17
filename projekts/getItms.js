501f624b80a59e58569effa6bb0cf7ad39c0099e
kodealt <shineyubhandari9@gmail.com>
2026-08-17 00:19:48 -0400

    work on some 3d card and lighting

--- projekts/getItms.js
+++ projekts/getItms.js
@@ -1,6 +1,4 @@
-// get shit from profile and post here.
-// make like hoverable shit and little previews of md, some images if possible
-//
+
 function buildCard(title, description){
     const container = document.getElementById('holder');
     const card = document.createElement('div');
@@ -14,6 +12,23 @@ function buildCard(title, description){
     card.appendChild(titleText);
     card.appendChild(desc);
     container.appendChild(card);
-}
+    card.addEventListener('mousemove', (e)=> {
+        const rect = card.getBoundingClientRect();
+        const width = rect.width;
+        const height = rect.height;
 
+        const mouseX = e.clientX - rect.left;
+        const mouseY = e.clientY - rect.top;
+        const xValue = (mouseX / width) - 0.5;
+        const yValue = (mouseY / height) - 0.5;
+        const maxRotation = 40;
+        const rotateX = yValue * -maxRotation;
+        const rotateY = xValue * maxRotation;
+        card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+        card.style.boxShadow = `${-rotateY *0.2}px ${rotateX * 0.2}px 10px rgba(0200,75,75,0.6)`;
+
+    card.style.setProperty('--mouse-x', `${mouseX}px`);
+    card.style.setProperty('--mouse-y', `${mouseY}px`);
+    })
+}
 buildCard('projekt00', 'this site')
